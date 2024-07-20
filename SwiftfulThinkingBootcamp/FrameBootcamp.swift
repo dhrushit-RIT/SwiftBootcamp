2e4358006e258ac42e6ebf1e885b5e65c42a678e
Dhrushit Raval <dr9703@rit.edu>
2024-07-20 16:47:27 -0700

    more bootcamps

---
+++ SwiftfulThinkingBootcamp/FrameBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  FrameBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by Dhrushit Raval on 7/19/24.
+//
+
+import SwiftUI
+
+struct FrameBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FrameBootcamp()
+}
---
+++ SwiftfulThinkingBootcamp/ImageBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  ImageBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by Dhrushit Raval on 7/19/24.
+//
+
+import SwiftUI
+
+struct ImageBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImageBootcamp()
+}
