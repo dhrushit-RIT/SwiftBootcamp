21c76b31dd1e5f27fd174aebd315c5006743aa59
Dhrushit Raval <dr9703@rit.edu>
2024-07-20 17:22:05 -0700

    spacers

---
+++ SwiftfulThinkingBootcamp/SpacerBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  SpacerBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by Dhrushit Raval on 7/20/24.
+//
+
+import SwiftUI
+
+struct SpacerBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SpacerBootcamp()
+}
