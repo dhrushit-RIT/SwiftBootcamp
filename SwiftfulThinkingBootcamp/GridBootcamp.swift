92afb12508f0262aba32dbb9e169a13093d883e3
Dhrushit Raval <dr9703@rit.edu>
2024-07-20 20:18:50 -0700

    scroll view bootcamp

---
+++ SwiftfulThinkingBootcamp/GridBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  GridBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by Dhrushit Raval on 7/20/24.
+//
+
+import SwiftUI
+
+struct GridBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GridBootcamp()
+}
---
+++ SwiftfulThinkingBootcamp/ScrollViewBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  ScrollViewBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by Dhrushit Raval on 7/20/24.
+//
+
+import SwiftUI
+
+struct ScrollViewBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScrollViewBootcamp()
+}
